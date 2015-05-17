obsproject/libdshowcapture

commit 880414b78ed0401beb3036f6e05dd974d13c7c61

Find and connect crossbar when directly connecting
When directly connecting the pins of the device filters to the capture
filters, it would not take in to account the potential of a crossbar
being required for the device.

This is important because most video capture devices (sans webcams)
require the use of crossbars in order to operate properly.

source/device.cpp
@@ -534,15 +534,46 @@ bool HDevice::CreateGraph()
 	return true;
 }
 
+bool HDevice::FindCrossbar(IBaseFilter *filter, IBaseFilter **crossbar)
+{
+	ComPtr<IPin> pin;
+	REGPINMEDIUM medium;
+	HRESULT hr;
+
+	hr = builder->FindInterface(NULL, NULL, filter, IID_IAMCrossbar,
+			(void**)crossbar);
+	if (SUCCEEDED(hr))
+		return true;
+
+	if (!GetPinByName(filter, PINDIR_INPUT, nullptr, &pin))
+		return false;
+	if (!GetPinMedium(pin, medium))
+		return false;
+	if (!GetFilterByMedium(AM_KSCATEGORY_CROSSBAR, medium, crossbar))
+		return false;
+
+	graph->AddFilter(*crossbar, L"Crossbar Filter");
+	return true;
+}
+
 bool HDevice::ConnectPins(const GUID &category, const GUID &type,
 		IBaseFilter *filter, CaptureFilter *capture)
 {
 	HRESULT hr;
+	ComPtr<IBaseFilter> crossbar;
 	ComPtr<IPin> filterPin;
 
 	if (!EnsureInitialized(L"HDevice::ConnectPins") ||
 	    !EnsureInactive(L"HDevice::ConnectPins"))
 		return false;
+
+	if (type == MEDIATYPE_Video && FindCrossbar(filter, &crossbar)) {
+		if (!DirectConnectFilters(graph, crossbar, filter)) {
+			Warning(L"HDevice::ConnectPins: Failed to connect "
+			        L"crossbar");
+			return false;
+		}
+	}
 
 	if (!GetFilterPin(filter, type, category, PINDIR_OUTPUT, &filterPin)) {
 		Error(L"HDevice::ConnectPins: Failed to find pin");

source/device.hpp
@@ -99,6 +99,7 @@ struct HDevice {
 	bool SetAudioConfig(AudioConfig *config);
 
 	bool CreateGraph();
+	bool FindCrossbar(IBaseFilter *filter, IBaseFilter **crossbar);
 	bool ConnectPins(const GUID &category, const GUID &type,
 			IBaseFilter *filter, CaptureFilter *capture);
 	bool RenderFilters(const GUID &category, const GUID &type,

commit 4ae7a5b8468fa92ddc7e72fd76772f237bbea075

Fix crash with encoded devices
The code introduced in 880414b78ed0401beb3036f6e05dd974d13c7c61 caused a
conflict with encoded devices because crossbars on encoded devices are
already connected.

source/device.cpp
@@ -562,12 +562,13 @@ bool HDevice::ConnectPins(const GUID &category, const GUID &type,
 	HRESULT hr;
 	ComPtr<IBaseFilter> crossbar;
 	ComPtr<IPin> filterPin;
+	bool connectCrossbar = !encodedDevice && type == MEDIATYPE_Video;
 
 	if (!EnsureInitialized(L"HDevice::ConnectPins") ||
 	    !EnsureInactive(L"HDevice::ConnectPins"))
 		return false;
 
-	if (type == MEDIATYPE_Video && FindCrossbar(filter, &crossbar)) {
+	if (connectCrossbar && FindCrossbar(filter, &crossbar)) {
 		if (!DirectConnectFilters(graph, crossbar, filter)) {
 			Warning(L"HDevice::ConnectPins: Failed to connect "
 			        L"crossbar");

source/device.hpp
@@ -61,6 +61,7 @@ struct HDevice {
 	VideoConfig                    videoConfig;
 	AudioConfig                    audioConfig;
 
+	bool                           encodedDevice = false;
 	bool                           initialized;
 	bool                           active;
 

source/dshow-encoded-device.cpp
@@ -246,6 +246,7 @@ bool HDevice::SetupEncodedVideoCapture(IBaseFilter *filter,
 		success = MapPacketIDs(demuxer, info.videoPacketID,
 				info.audioPacketID);
 
+	encodedDevice = success;
 	return success;
 }
 