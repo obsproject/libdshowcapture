60a06c2da36af5eae8a7ee51d71d35ce9c5410bc
jp9000 <obs.jim@gmail.com>
2019-06-23 20:52:31 -0700

    Move external header to dedicated directory
    Prevents clang-format from formatting the file automatically

--- source/dshow-enum.cpp
+++ source/dshow-enum.cpp
@@ -28,7 +28,7 @@
 	EXTERN_C const GUID DECLSPEC_SELECTANY name = {              \
 		l, w1, w2, {b1, b2, b3, b4, b5, b6, b7, b8}}
 
-#include "IVideoCaptureFilter.h"
+#include "external/IVideoCaptureFilter.h"
 
 namespace DShow {
 using namespace std;
