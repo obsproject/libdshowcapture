459b857e4ce4f61462bc5ed145302c586bb89a20
Richard Stanway <r.stanway@gmail.com>
2021-08-18 22:57:43 +0200

    Fix log level of DebugHR
    Detected by PVS Studio.

--- source/log.cpp
+++ source/log.cpp
@@ -90,7 +90,7 @@ void InfoHR(const wchar_t *str, HRESULT hr)
 
 void DebugHR(const wchar_t *str, HRESULT hr)
 {
-	Info(L"%s (0x%08lX): %s", str, hr, ConvertHRToEnglish(hr).c_str());
+	Debug(L"%s (0x%08lX): %s", str, hr, ConvertHRToEnglish(hr).c_str());
 }
 
 }; /* namespace DShow */
