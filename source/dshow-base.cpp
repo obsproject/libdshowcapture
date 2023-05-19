7a2cd192431013f1ff18b7cb6a377af7a74c44b4
Lain <lain@obsproject.com>
2023-05-18 17:49:34 -0700

    Update copyrights/names

--- source/dshow-base.cpp
+++ source/dshow-base.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014 Hugh Bailey <obs.jim@gmail.com>
+ *  Copyright (C) 2014 Lain Bailey <lain@obsproject.com>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
